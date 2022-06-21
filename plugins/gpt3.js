77517f069fb1db247c1a7cd292169962a99e25cd
John Parker <john.henry.parker@gmail.com>
2022-06-21 18:03:41 -0500

    GPT3

---
+++ plugins/gpt3.js
@@ -0,0 +1,46 @@
+const Axios = require('axios');
+
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'You must provide a message!'
+  var data = JSON.stringify({
+    "prompt": "Bot: Hello, I am an intelligent robot, tell me something and I'll do my best to answer.\n\nHuman:" + text + "\nBot:",
+    "max_tokens": 128,
+    "temperature": 0.85,
+    "top_p": 1,
+    "n": 1,
+    "stream": false,
+    "logprobs": null,
+    "stop": "\n",
+    "frequency_penalty": 0.6,
+    "best_of": 1,
+    "model": "text-davinci-002",
+  });
+
+  var config = {
+    method: 'post',
+    url: 'https://api.openai.com/v1/completions',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + process.env.OPENAI_API_KEY
+    },
+    data: data
+  };
+
+  await Axios(config)
+    .then(function (response) {
+      conn.sendMessage(m.chat, response.data.choices[0].text);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  /*conn.sendFile(m.chat, global.API('https://some-random-api.ml', '/canvas/youtube-comment', {
+    avatar: await conn.profilePictureUrl(m.sender).catch(_ => ''),
+    comment: text,
+    username: m.pushName
+  }), 'yt-comment.png', 'Here is your comment', m)*/
+}
+
+handler.command = /^(tellme)$/i
+
+module.exports = handler
