631cbabfdc3da7bd2cca8eddb8d674415e0944aa
John Parker <john.henry.parker@gmail.com>
2022-06-21 19:45:35 -0500

    GPT testing

--- plugins/gpt3.js
+++ plugins/gpt3.js
@@ -43,8 +43,8 @@ let handler = async (m, { conn, text }) => {
   }
 }
 
-handler.command = /^(tellme)$/i
-//handler.customPrefix = /[a-zA-z]/s
-//handler.command = new RegExp
+//handler.command = /^(tellme)$/i
+handler.customPrefix = /(bile?k|ban?h|cum?|knt?l|y?|mmk|p|b(a|i)?c?(o|i)?(t|d)?|wibu|p(a)?nt(e)?k|pepe?k)/i
+handler.command = new RegExp
 
 module.exports = handler
